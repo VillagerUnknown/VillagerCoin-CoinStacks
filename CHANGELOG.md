d58c15800dd855b8dc9c6a9a88245180975ad10a
villagerunknown <VillagerUnknown@proton.me>
2025-05-01 12:24:04 -0400

    Medium coin stacks can now merge to form Large stacks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,12 +7,15 @@ All notable changes to this project will be documented in this file.
 ### Added
 
 - Added recipes for Coin Stacks allowing them to be crafted in Survival mode.
-- Added option to Villager Coin to toggle the ability to break Coin Stacks by walking over them. _This is now disabled by default._
+- Added Slab and Full Block Coin Stacks for Copper, Iron, Gold, Emerald, and Netherite.
+- Added option to Villager Coin to toggle the ability to break Coin Stacks by walking over them. 
+_This is now disabled by default._
 
 ### Changed
 
+- Changed Medium Coin Stacks to allow them to merge when placed in the same block. (Like a slab.)
 - Changed Emerald and Netherite Coin Stack values to be accurate to their conversions. 
-They originally had a lower value for mapmakers but have been adjusted for crafting.
+_They originally had a lower value for mapmakers but have been adjusted for crafting._
 - Changed mixin filenames to avoid potential conflicts.
 
 ## [1.0.0]
--- src/main/java/me/villagerunknown/coinstacks/block/MediumCoinStackBlock.java
+++ src/main/java/me/villagerunknown/coinstacks/block/MediumCoinStackBlock.java
@@ -1,16 +1,30 @@
 package me.villagerunknown.coinstacks.block;
 
 import com.google.common.collect.ImmutableMap;
+import me.villagerunknown.villagercoin.block.entity.AbstractCurrencyValueBlockEntity;
+import me.villagerunknown.villagercoin.component.CurrencyComponent;
+import me.villagerunknown.villagercoin.feature.CoinFeature;
+import me.villagerunknown.villagercoin.feature.CoinStackBlocksFeature;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 import java.util.HashMap;
 import java.util.function.Function;
+
+import static me.villagerunknown.villagercoin.Villagercoin.CURRENCY_COMPONENT;
 
 public class MediumCoinStackBlock extends CoinStackBlock {
 	
@@ -18,6 +32,56 @@ public class MediumCoinStackBlock extends CoinStackBlock {
 	
 	public MediumCoinStackBlock(Settings settings) {
 		super(settings);
+	}
+	
+	@Override
+	protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		if( stack.isOf( this.asItem() ) ) {
+			CurrencyComponent currencyComponent = stack.get( CURRENCY_COMPONENT );
+			
+			if( null != currencyComponent ) {
+				BlockState blockState = null;
+				long value = currencyComponent.value();
+				long newValue = 0;
+				
+				if( CoinStackBlocksFeature.COPPER_VALUE * CoinStackBlocksFeature.MEDIUM_VALUE_MULTIPLIER == value ) {
+					blockState = CoinStackBlocks.LARGE_COPPER_COIN_STACK.getDefaultState();
+					newValue = CoinStackBlocksFeature.COPPER_VALUE * CoinStackBlocksFeature.LARGE_VALUE_MULTIPLIER;
+				} else if( CoinStackBlocksFeature.IRON_VALUE * CoinStackBlocksFeature.MEDIUM_VALUE_MULTIPLIER == value ) {
+					blockState = CoinStackBlocks.LARGE_IRON_COIN_STACK.getDefaultState();
+					newValue = CoinStackBlocksFeature.IRON_VALUE * CoinStackBlocksFeature.LARGE_VALUE_MULTIPLIER;
+				} else if( CoinStackBlocksFeature.GOLD_VALUE * CoinStackBlocksFeature.MEDIUM_VALUE_MULTIPLIER == value ) {
+					blockState = CoinStackBlocks.LARGE_GOLD_COIN_STACK.getDefaultState();
+					newValue = CoinStackBlocksFeature.GOLD_VALUE * CoinStackBlocksFeature.LARGE_VALUE_MULTIPLIER;
+				} else if( CoinStackBlocksFeature.EMERALD_VALUE * CoinStackBlocksFeature.MEDIUM_VALUE_MULTIPLIER == value ) {
+					blockState = CoinStackBlocks.LARGE_EMERALD_COIN_STACK.getDefaultState();
+					newValue = CoinStackBlocksFeature.EMERALD_VALUE * CoinStackBlocksFeature.LARGE_VALUE_MULTIPLIER;
+				} else if( CoinStackBlocksFeature.NETHERITE_VALUE * CoinStackBlocksFeature.MEDIUM_VALUE_MULTIPLIER == value ) {
+					blockState = CoinStackBlocks.LARGE_NETHERITE_COIN_STACK.getDefaultState();
+					newValue = CoinStackBlocksFeature.NETHERITE_VALUE * CoinStackBlocksFeature.LARGE_VALUE_MULTIPLIER;
+				} // if, else if ...
+				
+				if( null != blockState ) {
+					CoinFeature.playHeavyCoinSound( player );
+					
+					world.setBlockState(pos, blockState.with(FACING, player.getHorizontalFacing()).with( WATERLOGGED, world.getFluidState( pos ).isOf( Fluids.WATER ) ) );
+					
+					BlockEntity blockEntity = world.getBlockEntity(pos);
+					
+					if( blockEntity instanceof AbstractCurrencyValueBlockEntity currencyValueBlockEntity ) {
+						currencyValueBlockEntity.setTotalCurrencyValue( newValue );
+					} // if
+					
+					world.markDirty(pos);
+					
+					stack.decrementUnlessCreative( 1, player );
+					
+					return ItemActionResult.SUCCESS;
+				} // if
+			} // if
+		}
+		
+		return super.onUseWithItem(stack, state, world, pos, player, hand, hit);
 	}
 	
 	@Override

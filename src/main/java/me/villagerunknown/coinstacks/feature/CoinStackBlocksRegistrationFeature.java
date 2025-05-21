b7333344f673fd63064b192007e49bd5e53f108c
villagerunknown <VillagerUnknown@proton.me>
2025-05-20 22:41:24 -0400

    renaming feature

--- src/main/java/me/villagerunknown/coinstacks/feature/CoinStackBlocksRegistrationFeature.java
+++ src/main/java/me/villagerunknown/coinstacks/feature/CoinStackBlocksRegistrationFeature.java
@@ -1,27 +1,29 @@
 package me.villagerunknown.coinstacks.feature;
 
 import me.villagerunknown.coinstacks.block.*;
+import me.villagerunknown.villagercoin.Villagercoin;
 import me.villagerunknown.villagercoin.block.CoinStackBlock;
+import me.villagerunknown.villagercoin.feature.CoinStackBlocksFeature;
 import me.villagerunknown.villagercoin.type.CoinType;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 
-public class CoinStackBlocksFeature {
+public class CoinStackBlocksRegistrationFeature {
 
 	public static void execute(){
 		new CoinStackBlocks();
 	}
 	
 	public static Block registerCoinStackBlock( String id, Block block, long value ) {
-		return me.villagerunknown.villagercoin.feature.CoinStackBlocksFeature.registerCoinStackBlock( id, block, value );
+		return CoinStackBlocksFeature.registerCoinStackBlock( Villagercoin.MOD_ID, id, block, value );
 	}
 	
 	public static Block registerCraftableCoinStackBlock( CoinType type, String id, Block block, long value ) {
-		return me.villagerunknown.villagercoin.feature.CoinStackBlocksFeature.registerCraftableCoinStackBlock( type, id, block, value );
+		return CoinStackBlocksFeature.registerCraftableCoinStackBlock( type, Villagercoin.MOD_ID, id, block, value );
 	}
 	
 	public static Block registerCraftableFireproofCoinStackBlock( CoinType type, String id, Block block, long value ) {
-		return me.villagerunknown.villagercoin.feature.CoinStackBlocksFeature.registerCraftableFireproofCoinStackBlock( type, id, block, value );
+		return CoinStackBlocksFeature.registerCraftableFireproofCoinStackBlock( type, Villagercoin.MOD_ID, id, block, value );
 	}
 	
 	public static Block registerSmallCoinStackBlock( CoinType type, String id, long value ) {
